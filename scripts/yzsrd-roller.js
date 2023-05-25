dbf87d99bf2f30070d8dc02089aee0d9a1f5a517
Chris Clouser <waryjack@gmail.com>
2023-05-25 10:15:50 -0400

    basic stuff, nothing usable yet

--- scripts/yzsrd-roller.js
+++ scripts/yzsrd-roller.js
@@ -1,9 +1,25 @@
+import { YZRoll } from "./yzroll.js";
+
 console.log("Hello World! This code runs immediately when the file is loaded.");
 
 Hooks.on("init", function() {
   console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
 });
 
-Hooks.on("ready", function() {
+Hooks.once("ready", function() {
   console.log("This code runs once core initialization is ready and game data is available.");
+
+  const diceIconSelector = '#chat-controls i.fas.fa-dice-d20';
+    $(document).on('click', diceIconSelector, () => { 
+        YZRoll.prompt();
+    });
+
+});
+
+Hooks.on('renderChatMessage', (app, html) => {
+
+    html.on('click', '.push-roll', event => {
+        YZRoll.push();
+     });
+
 });
